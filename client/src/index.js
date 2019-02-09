c829877d1d34fe12f47a2f58bfd866481a8d67c3
barneslardo <37343575+barneslardo@users.noreply.github.com>
2019-02-09 10:12:09 -0800

    modify for deploy

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./main.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-serviceWorker.register();
 if (module.hot) module.hot.accept();
 console.log(
   "console log is showing this " + process.env.REACT_APP_CALLBACK_URI
--- index.js
+++ index.js
@@ -46,7 +46,15 @@ epilogue.resource({
   endpoints: ["/posts", "/posts/:id"]
 });
 
-const PORT = process.env.SERVER_PORT || 4000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.SERVER_PORT || 3006;
 
 database.sync().then(() => {
   app.listen(PORT, () => {
