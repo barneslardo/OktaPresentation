781ba20f213332505658abe2f51cf39636572007
barneslardo <37343575+barneslardo@users.noreply.github.com>
2019-02-08 20:45:52 -0800

    changed port var

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ epilogue.resource({
   endpoints: ["/posts", "/posts/:id"]
 });
 
-const PORT = process.env.SERVER_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen = PORT;
 console.log("Server is running");
