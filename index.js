89b58e13a18e5ccc18e1f2d685816cb275993957
barneslardo <37343575+barneslardo@users.noreply.github.com>
2019-02-08 20:12:56 -0800

    tweak

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ const oktaJwtVerifier = new OktaJwtVerifier({
   issuer: "https://bigfootwebservice.okta.com/oauth2/ausacu2zhRbgylKNP356"
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
