63e533665c1cb8aee4b3c6d131c3696337bc725c
barneslardo <37343575+barneslardo@users.noreply.github.com>
2019-02-08 19:32:07 -0800

    small change

--- index.js
+++ index.js
@@ -14,29 +14,13 @@ const oktaJwtVerifier = new OktaJwtVerifier({
   issuer: process.env.REACT_APP_OKTA_ORG_URL
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-// mongoose.Promise = global.Promise;
-// mongoose
-//   .connect(
-//     process.env.REACT_APP_MONGO_URI,
-//     { useNewUrlParser: true }
-//   )
-//   .then(
-//     () => {
-//       console.log("Mongo is connected");
-//     },
-//     err => {
-//       console.log("Cannot connect to Mongo" + err);
-//     }
-//   );
-
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
